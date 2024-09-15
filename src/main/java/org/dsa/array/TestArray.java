da27132066ed7b41780a2c2c32ad3ad256b2b1e0
yvsweng <47563577+yyashcit@users.noreply.github.com>
2024-09-15 12:29:55 +0530

    my first commit

---
+++ src/main/java/org/dsa/array/TestArray.java
@@ -0,0 +1,8 @@
+package org.dsa.array;
+
+public class TestArray {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+        System.out.println("hello ");
+    }
+}
